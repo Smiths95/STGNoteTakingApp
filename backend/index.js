cfa1c53032000c1b483010c06471250c6aad73e3
Gang Cao <gangcao@Gangs-MacBook-Pro.local>
2022-12-06 21:34:38 -0500

    did signup work, will need work on add and delete

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,54 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const path = require('path');
+const express = require('express')
+const mongoose = require('mongoose')
+const Note = require('./models/Note')
+const User = require('./models/User')
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+const port = 3000
+mongoose.connect('mongodb://localhost:27017/NotesApp');
+//endpoints to server the HTML
+app.get('/', (req, res) => {
+  res.sendFile("pages/index.html",{root: __dirname})
+})
 
-const usersRouter = require('./routes/users.route');
-const notesRouter = require('./routes/notes.route');
+app.get('/login', (req, res) => {
+    res.sendFile("pages/login.html",{root: __dirname})
+  })
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.use(cors());
-app.use(express.json());
-
-const dbURL =  "mongodb://localhost:27017";
-
-mongoose
-    .connect(process.env.MONGODB_URI ||dbURL,{ useUnifiedTopology:true, useNewUrlParser: true })
-    .then(() => console.log("MongoDB successfully connected"))
-    .catch(err => console.log(err));
-    
-mongoose.set('useCreateIndex', true);
-
-app.use('/users',usersRouter);
-app.use('/notes',notesRouter);
-
-if(process.env.NODE_ENV === 'production') {
+app.get('/signup', (req, res) => {
+    res.sendFile("pages/signup.html",{root: __dirname})
+  })
+ //endpoints for APIs
+app.post('/getnotes',async (req, res) => {
+  let notes = await Note.find({email: req.body.email})
+    res.status(200).json({success:true, notes})
+})
+app.post('/login', async(req, res) => {
+    let user = await User.findOne(req.body)
+    console.log(user)
+   if(!user){
+    res.status(200).json({success: false,message: "Nouesr found"})
+   }
+   else{
+    res.status(200).json({success: true, message: "uesr found"})
+   }
    
-    app.use(express.static(path.join(__dirname, "client", "build")))
-
-    app.get('*',(req, res) => {
-        res.sendFile(path.join(__dirname,'client','build','index.html'));
-    });
-}
-
-app.listen(port,()=>{
-    console.log(`Server is running on port:${port}`);
-});
+})
+app.post('/signup', async(req, res) => {
+    const {userToken} = req.body
+    let user = await User.create(req.body)
+    res.status(200).json({success:true, user: user})
+})
+app.post('/addnote', async(req, res) => {
+    const {userToken} = req.body
+    let user = await Note.create(req.body)
+    res.status(200).json({success:true, note})
+})
+app.post('/deletenote', (req, res) => {
+    const {userToken} = req.body
+    res.sendFile("pages/signup.html",{root: __dirname})
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
